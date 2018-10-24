ee6a3fe74fb42daa372d2c8feec743ab7c3c44e7
lucien.moor <lucien.moor@he-arc.ch>
2018-10-24 11:57:43 +0200

    updated pom.xml

--- SpringTestDemo/src/main/java/com/example/demo/models/User.java
+++ SpringTestDemo/src/main/java/com/example/demo/models/User.java
@@ -1,18 +1,24 @@
 package com.example.demo.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "email")
     private String email;
 
 	public Integer getId() {
