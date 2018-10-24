710cb9727c8c8a45e0f5b358d5839514c45ac169
lucien.moor <lucien.moor@he-arc.ch>
2018-10-24 13:47:00 +0200

    re-enable test

--- SpringTestDemo/src/test/java/com/example/demo/SpringTestDemoApplicationTests.java
+++ SpringTestDemo/src/test/java/com/example/demo/SpringTestDemoApplicationTests.java
@@ -18,13 +18,13 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;*/
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.models.User;
 
@@ -32,24 +32,24 @@ import com.example.demo.models.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)//use random port to test
 @EnableAutoConfiguration(exclude=HibernateJpaAutoConfiguration.class)
-//@AutoConfigureWebTestClient
-//@AutoConfigureMockMvc
+@AutoConfigureWebTestClient
+@AutoConfigureMockMvc
 public class SpringTestDemoApplicationTests {
 
-	/*@Autowired
+	@Autowired
 	private WebTestClient webClient;
 	
 
     @Autowired
     private MockMvc mockMvc;
-    */
+    
     @Test
     public void contextLoads() throws Exception {
     }
 	
 	@Test
 	public void homeTest() throws Exception {
-		//this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Get your greeting")));
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Get your greeting")));
 		
 		
 	}
@@ -57,9 +57,9 @@ public class SpringTestDemoApplicationTests {
 	@Test
 	public void addUserTest() throws Exception {
 		String name = "toto";
-		/*this.webClient.get().uri("/add?name="+name).exchange().expectStatus().isOk();
+		this.webClient.get().uri("/add?name="+name).exchange().expectStatus().isOk();
 		List<User> users = this.webClient.get().uri("/all").exchange().expectBodyList(User.class).returnResult().getResponseBody();
-		Assert.assertTrue(users.stream().anyMatch(elem -> elem.getName().equals(name)));*/
+		Assert.assertTrue(users.stream().anyMatch(elem -> elem.getName().equals(name)));
 		
 	}
 
