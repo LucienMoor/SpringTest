b803b46c611a3204c78e38ad8d0da7746929bafd
LucienMoor <lucien.moor@gmail.com>
2018-10-25 16:12:21 +0200

    Update katalon.sh

--- src/setup/katalon.sh
+++ src/setup/katalon.sh
@@ -10,12 +10,8 @@ package=Katalon_Studio_Linux_64-$version.tar.gz
 unzipped_directory=Katalon_Studio_Linux_64-$version
 
 wget -O $package http://download.katalon.com/$directory/$package
-tar -xvzf $package -C $KATALON_KATALON_INSTALL_DIR_PARENT
+tar -xvzf $package -C /katalon
 ls
 rm $package
-
-mv $KATALON_KATALON_INSTALL_DIR_PARENT/$unzipped_directory $KATALON_KATALON_INSTALL_DIR
-chmod u+x $KATALON_KATALON_INSTALL_DIR/katalon
-chmod u+x $KATALON_KATALON_INSTALL_DIR/configuration/resources/drivers/chromedriver_linux64/chromedriver
 
 echo "Katalon Studio $version" >> $KATALON_VERSION_FILE
