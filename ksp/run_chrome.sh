a6e6d9155b32b7f8e0a0bfe4f22bb25da838e1e8
LucienMoor <lucien.moor@gmail.com>
2018-10-25 14:52:28 +0200

    Update run_chrome.sh

--- ksp/run_chrome.sh
+++ ksp/run_chrome.sh
@@ -10,6 +10,6 @@ rm -rfv "$reports_dir"
 mkdir -p "$reports_dir"
 
 katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
-docker run --rm -v "$project_dir":/katalon/katalon/source:ro -v "$reports_dir":/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
+docker run --rm -u root -v "$project_dir":/katalon/katalon/source -v "$reports_dir":/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
 
 cd $current_dir
