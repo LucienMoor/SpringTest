ba083710ba0d0f6c66c739161f2d721d60ed5126
LucienMoor <lucien.moor@gmail.com>
2018-10-26 12:53:27 +0200

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -1,1 +1,1 @@
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="/var/jenkins_home/workspace/DemoPipeline/SpringTest/SpringTest.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Chrome (headless)"
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="/var/jenkins_home/workspace/DemoPipeline/SpringTest/SpringTest.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
