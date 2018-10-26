634dbe4b90f59b8f8f94a8be2389145e2a6e96ba
LucienMoor <lucien.moor@gmail.com>
2018-10-26 13:20:12 +0200

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+ 
+bash 'Xvfb :99 &'
+sh 'export DISPLAY=:99'
+sh 'xhost +'
+
 /Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="/var/jenkins_home/workspace/DemoPipeline/SpringTest/SpringTest.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
