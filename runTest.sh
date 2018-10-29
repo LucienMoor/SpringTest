ffb2ee3fda0c5259b3564c9c4e8c6520060ae6d4
LucienMoor <lucien.moor@gmail.com>
2018-10-29 08:23:25 +0100

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 Xvfb :99 &
-export local_addr="localhost:8888"
+export local_addr="127.0.0.1:8888"
 
 /Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="/var/jenkins_home/workspace/DemoPipeline/SpringTest/SpringTest.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
