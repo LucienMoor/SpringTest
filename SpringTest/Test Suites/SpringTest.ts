39ea89064394f8f262fd8186c7590fae589025fe
lucien.moor <lucien.moor@he-arc.ch>
2018-11-09 09:04:26 +0100

    try fixing test suite problem

--- SpringTest/Test Suites/SpringTest.ts
+++ SpringTest/Test Suites/SpringTest.ts
@@ -13,19 +13,19 @@
    <testCaseLink>
       <guid>8ee0ea2e-e267-4911-9597-f907ae8c4bb1</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Try index</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4df2b4b7-d5b0-43a6-af0e-6129a9aa9fe6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/registration</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4844288a-e693-490b-a141-2013208c20d3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
       <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4df2b4b7-d5b0-43a6-af0e-6129a9aa9fe6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/registration</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
